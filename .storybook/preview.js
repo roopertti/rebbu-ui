d09ca6857d4e58c239275ab8f8de04553b0e2b6a
Robert Kuhlmann <robert.kuhlmann2@gmail.com>
2021-09-12 20:13:24 +0300

    initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import 'normalize.css'
+import '../styles/theme.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+}
